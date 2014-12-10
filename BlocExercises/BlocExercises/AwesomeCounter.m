2a6ea9bf818e56fe4c650b7395dc3c50f13e70fb
Steve Palley <spall03@gmail.com>
2014-12-10 11:44:46 -0800

    Completed loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,35 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    
+    NSInteger larger = 0;
+    NSInteger smaller = 0;
+    
+    //first we figure out which number is smaller
+    
+    if (number <= otherNumber)
+    {
+        larger = otherNumber;
+        smaller = number;
+    }
+    else
+    {
+        larger = number;
+        smaller = otherNumber;
+    }
+    
+    //then we build the string
+    
+    NSMutableString *result = [[NSMutableString alloc]init];
+    
+    while (smaller <= larger ) //needs to handle the smaller == larger case
+    {
+        NSString *temp = [NSString stringWithFormat:@"%ld", smaller];
+        [result appendString:temp];
+        smaller++;
+    }
+    
+    return result;
 }
 
 @end
