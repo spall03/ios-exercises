f3afa1ce53fe76132c7ff486afef874c0ccebd3e
Steve Palley <spall03@gmail.com>
2014-12-10 17:18:08 -0800

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,54 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    
+    NSInteger unboxednumber = [number integerValue]; //get the int out of the NSNumber
+    NSNumber *result = [NSNumber numberWithInt:(unboxednumber*2)]; //do the math and wrap it again
+    
+    return result;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *result = [NSMutableArray new]; //empty array for result
+    NSInteger bigger = 0; //comparison empties
+    NSInteger smaller = 0;
+    
+    if (number >= otherNumber) //decide which is bigger
+    {
+        bigger = number;
+        smaller = otherNumber;
+    }
+    else
+    {
+        bigger = otherNumber;
+        smaller = number;
+    }
+    
+    for (NSInteger i = smaller; i <= bigger; i++) //must handle equal numbers
+    {
+        NSNumber *temp = [NSNumber numberWithInt:i];
+        [result addObject:temp];
+    }
+    
+    return result;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    
+    NSNumber *lowest = arrayOfNumbers[0]; //start with first number in array
+    
+    for (NSNumber *tester in arrayOfNumbers) //now loop through and compare
+    {
+        if ([lowest compare:tester]==NSOrderedDescending) //class comparison method
+        {
+            lowest = tester; //replace if lower
+        }
+        
+    }
+    
+    
+    return [lowest integerValue]; //re-wrap as NSInteger
 }
 
 @end
