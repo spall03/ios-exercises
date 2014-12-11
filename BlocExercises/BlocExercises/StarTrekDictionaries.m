a42f24d26272f108ae60f1b05fc1157f5903ac01
Steve Palley <spall03@gmail.com>
2014-12-10 16:27:21 -0800

    Completed dictionaries

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -11,18 +11,35 @@
 @implementation StarTrekDictionaries
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
-    return @"";
+    
+    NSString *drink = characterDictionary[@"favorite drink"]; //access the proper key
+    return drink;
+    
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
-    /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *drinks = [[NSMutableArray alloc]init]; //empty array to be filled
+    
+    for (NSDictionary *character in charactersArray) //iterate through array of dictionaries
+    {
+        NSString *temp = character[@"favorite drink"]; //access each dict's favorite drink
+        [drinks addObject:temp]; //append to empty array
+    }
+    
+    return drinks;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
-    return @{};
+    
+    NSMutableDictionary *newDict = [characterDictionary mutableCopy]; //cloning input dict as mutable
+    
+    if (characterDictionary[@"quote"] == nil) //only add quote if input dict doesn't already have one
+    {
+        [newDict setObject:@"Reports of my assimilation are greatly exaggerated." forKey:@"quote"];
+    }
+   
+    return newDict;
 }
 
 @end
